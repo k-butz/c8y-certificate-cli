90619ec0bf52fd5fb8c6dbf517a820b2ec132d27
Korbinian Butz <korbinian.butz@cumulocity.com>
2025-06-08 15:59:25 +0200

    update version

--- pkg/version.go
+++ pkg/version.go
@@ -12,6 +12,6 @@ var versionCmdGroup CmdGroupVersion
 
 // TODO: include this into goreleaser
 func (g *CmdGroupVersion) Execute(args []string) error {
-	fmt.Println("c8y-certificate-cli version: 0.1")
+	fmt.Println("c8y-certificate-cli version: 0.1.0")
 	return nil
 }
